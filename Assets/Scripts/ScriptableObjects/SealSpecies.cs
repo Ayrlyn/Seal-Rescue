dc405aa66a6bf0ff862fa7d41eef7c1b8f28165b
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-03-30 10:41:19 +0100

    [ADD]Seal and World SOs

---
+++ Assets/Scripts/ScriptableObjects/SealSpecies.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Seals/Species")]
+public class SealSpecies : ScriptableObject
+{
+    [SerializeField] string speciesName;
+    [SerializeField] Vector2 birthLengthRange;
+    [SerializeField] Vector2 birghtWeightRange;
+    [SerializeField] List<Month> earlyPuppingMonths;
+    [SerializeField] List<Month> latePuppingMonths;
+    [SerializeField] List<Month> puppingMonths;
+    [SerializeField] Vector2 releaseWeightRange;
+    [SerializeField] Vector2 weaningTimeRange;
+}
