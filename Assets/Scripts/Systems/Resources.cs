cd49a725b4dba702319d1e4f3b7a453b2f6df673
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-03-30 10:39:37 +0100

    [ADD]Resources script

---
+++ Assets/Scripts/Systems/Resources.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : Singleton<Resources>
+{
+    #region local variables
+    int _food = 1000;
+    int _materials = 1000;
+    int _medicine = 1000;
+    int _money = 1000;
+    #endregion
+
+    #region getter and setters
+    public int Food { get { return _food; } }
+    public int Materials { get { return _materials; } }
+    public int Medicine { get { return _medicine; } }
+    public int Money { get { return _money; } }
+    #endregion
+
+    #region public methods
+    public bool SpendFood(int quantity)
+    {
+        if(quantity > Food) { return false; }
+        _food -= quantity;
+        return true;
+    }
+
+    public bool SpendMaterials(int quantity)
+    {
+        if (quantity > Materials) { return false; }
+        _materials -= quantity;
+        return true;
+    }
+
+    public bool SpendMedicine(int quantity)
+    {
+        if (quantity > Medicine) { return false; }
+        _medicine -= quantity;
+        return true;
+    }
+
+    public bool SpendMoney(int quantity)
+    {
+        if (quantity > Money) { return false; }
+        _money -= quantity;
+        return true;
+    }
+    #endregion
+}
