a731c17988dd96ed8ccb1a09586a53e818dca476
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-03-30 10:41:36 +0100

    [ADD]Seal entity Script

---
+++ Assets/Scripts/Entities/Seal.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seal : MonoBehaviour
+{
+    #region local variables
+    Tuple<int, int, int, int> _age;
+    KeyValuePair<Month, int> _birthday;
+    SealHealth _health;
+    float _hunger;
+    SealMood _mood;
+    string _name;
+    KeyValuePair<Month, int> _rescueDay;
+    SealRescueProgress _rescueProgress;
+    float _weight;
+    #endregion
+}
