b77f0b363eb991b763ebe46ed2314f8f1f5263e2
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-03-30 10:40:42 +0100

    [ADD]Date Time system

---
+++ Assets/Scripts/Systems/GameDateTime.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDateTime : Singleton<GameDateTime>
+{
+    #region editor variables
+    [Range(1, 5)]public int _timeScaleMultiplier = 1;
+    public float _timeToPassOneMinute = 0.5f;
+    #endregion
+
+    #region local variables
+    Day _currentDay = Day.Monday;
+    int _currentHour = 0;
+    int _currentMinute = 0;
+    KeyValuePair<Month, int> _currentMonth = new KeyValuePair<Month, int>(Month.January, 1);
+    float _minuteTimePassed = 0f;
+    Dictionary<Month, int> _months = new Dictionary<Month, int>()
+    {
+        { Month.January, 31 },
+        { Month.February, 28 },
+        {Month.March, 31 },
+        {Month.April, 30 },
+        {Month.May, 31 },
+        {Month.June, 30 },
+        {Month.July, 31 },
+        {Month.August, 31 },
+        {Month.September,30 },
+        {Month.October,31 },
+        { Month.November,30},
+        {Month.December, 31 }
+    };
+    #endregion
+
+    #region getters and setters
+    public Day CurrentDay { get { return _currentDay; } }
+    public int CurrentHour { get { return _currentHour; } }
+    public int CurrentMinute { get { return _currentMinute; } }
+    public KeyValuePair<Month, int> CurrentMonth { get { return _currentMonth; } }
+    #endregion
+
+    #region unity methods
+    void Update()
+    {
+        PassTime();
+    }
+    #endregion
+
+    #region local methods
+    void PassTime()
+    {
+        _minuteTimePassed += Time.deltaTime * _timeScaleMultiplier;
+        if(_minuteTimePassed < _timeToPassOneMinute) { return; }
+
+        _minuteTimePassed = 0;
+        _currentMinute++;
+        EventMessenger.Instance.SendTimeAndDateMessage(TimePassed.Minute);
+        if (_currentMinute < 60) { return; }
+
+        _currentMinute = 0;
+        _currentHour++;
+        EventMessenger.Instance.SendTimeAndDateMessage(TimePassed.Hour);
+        if(CurrentHour == 9) { SaveManager.Instance.Save(); }
+        if(_currentHour < 24) { return; }
+
+        _currentHour = 0;
+        EventMessenger.Instance.SendTimeAndDateMessage(TimePassed.Day);
+        if ((int)_currentDay < 7) { _currentDay = (Day)(int)(_currentDay + 1); }
+        else 
+        { 
+            _currentDay = (Day)1;
+            EventMessenger.Instance.SendTimeAndDateMessage(TimePassed.Week);
+        }
+
+        if (_currentMonth.Value < _months[_currentMonth.Key]) { _currentMonth = new KeyValuePair<Month, int>(_currentMonth.Key, _currentMonth.Value + 1); }
+        else
+        {
+            if ((int)_currentMonth.Key < 12) 
+            { 
+                _currentMonth = new KeyValuePair<Month, int>((Month)(int)_currentMonth.Key + 1, 1);
+                EventMessenger.Instance.SendTimeAndDateMessage(TimePassed.Month);
+            }
+            else 
+            { 
+                _currentMonth = new KeyValuePair<Month, int>((Month)1, 1);
+                EventMessenger.Instance.SendTimeAndDateMessage(TimePassed.Month);
+                EventMessenger.Instance.SendTimeAndDateMessage(TimePassed.Year);
+            }
+        }
+    }
+    #endregion
+
+    #region public methods
+    public void SetGameDateTime(Day day, int hour, int minute, KeyValuePair<Month, int> month)
+    {
+        _currentDay = day;
+        _currentHour = hour;
+        _currentMinute = minute;
+        _currentMonth = new KeyValuePair<Month, int>(month.Key, month.Value);
+    }
+    #endregion
+}
