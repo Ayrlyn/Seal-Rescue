58b5cac4d2c872d75cbf03d06ac178c710c2e18d
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-04-06 21:04:47 +0100

    [MOD]Task class with optional Seal component

--- Assets/Scripts/Entities/Task.cs
+++ Assets/Scripts/Entities/Task.cs
@@ -9,6 +9,7 @@ public class Task
     int _minutesRequired;
     int _quantityRequired;
     Resources _resourceRequired;
+    Seal _seal;
     #endregion
 
     #region getters and setters
@@ -16,13 +17,15 @@ public class Task
     public int MinutesRequired { get { return _minutesRequired; } }
     public int QuantityRequired { get { return _quantityRequired; } }
     public Resources ResourceRequired { get { return _resourceRequired; } }
+    public Seal Seal { get { return _seal; } }
     #endregion
 
-    public Task(int minutesRequired, int quantityRequired, Resources resourceRequired)
+    public Task(int minutesRequired, int quantityRequired, Resources resourceRequired, Seal seal = null)
     {
         _minutesRemaining = minutesRequired;
         _minutesRequired = minutesRequired;
         _quantityRequired = quantityRequired;
         _resourceRequired = resourceRequired;
+        _seal = seal;
     }
 }
