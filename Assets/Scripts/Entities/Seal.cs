0f8d3ecb093814c7e47d71548a01b080b2fc841c
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-04-07 19:32:56 +0100

    [MOD]Seal class with better data and constructor

--- Assets/Scripts/Entities/Seal.cs
+++ Assets/Scripts/Entities/Seal.cs
@@ -7,13 +7,14 @@ public class Seal : MonoBehaviour
 {
     #region local variables
     int _age;
-    KeyValuePair<Month, int> _birthday;
     SealHealth _health;
+    int _healthValue;
     float _hunger;
     SealMood _mood;
     string _name;
     KeyValuePair<Month, int> _rescueDay;
     SealRescueProgress _rescueProgress;
+    SealSpecies _sealSpecies;
     float _weight;
     #endregion
 
@@ -21,7 +22,23 @@ public class Seal : MonoBehaviour
     public int Age { get { return _age; } }
     public SealHealth Health { get { return _health; } }
     public SealMood Mood { get { return _mood; } }
+    public string Name { get { return _name; } }
     public KeyValuePair<Month, int> RescueDay { get { return RescueDay; } }
     public SealRescueProgress RescueProgress { get { return _rescueProgress; } }
+    public SealSpecies SealSpecies { get { return _sealSpecies; } }
+    #endregion
+
+    #region constructor
+    public Seal(int age, SealHealth health, int healthValue, SealMood mood, string name, SealRescueProgress rescueProgress, SealSpecies sealSpecies, float weight)
+    {
+        _age = age;
+        _health = health;
+        _healthValue = healthValue;
+        _mood = mood;
+        _name = name;
+        _rescueProgress = rescueProgress;
+        _sealSpecies = sealSpecies;
+        _weight = weight;
+    }
     #endregion
 }
