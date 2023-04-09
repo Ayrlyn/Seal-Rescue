cccb43382f479f46449fe88bdce74f39ae85a33e
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-04-09 10:02:21 +0100

    [MOD]Include SealSave in GameSave

--- Assets/Scripts/Systems/SaveLoad/GameSave.cs
+++ Assets/Scripts/Systems/SaveLoad/GameSave.cs
@@ -7,13 +7,15 @@ public class GameSave
     public DateTimeSave _dateTimeSave;
     public HashSet<string> _oneOffGameEvents;
     public ResourcesSave _resourcesSave;
+    public List<SealSave> _sealSaves;
     public UpkeepSave _upkeepSave;
 
-    public GameSave(DateTimeSave dateTimeSave, HashSet<string> oneOffGameEvents, ResourcesSave resourcesSave, UpkeepSave upkeepSave)
+    public GameSave(DateTimeSave dateTimeSave, HashSet<string> oneOffGameEvents, ResourcesSave resourcesSave, List<SealSave> sealSaves, UpkeepSave upkeepSave)
     {
         _dateTimeSave = dateTimeSave;
         _oneOffGameEvents = oneOffGameEvents;
         _resourcesSave = resourcesSave;
+        _sealSaves = sealSaves;
         _upkeepSave = upkeepSave;
     }
 }
