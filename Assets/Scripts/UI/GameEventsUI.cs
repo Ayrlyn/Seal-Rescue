43cfe428021d84fab2f23dad56c7102364b5fcad
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-04-09 21:24:51 +0100

    [MOD]GameEventUI creates button prefabs depending on event receives

--- Assets/Scripts/UI/GameEventsUI.cs
+++ Assets/Scripts/UI/GameEventsUI.cs
@@ -2,9 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameEventsUI : MonoBehaviour
+public class GameEventsUI : Singleton<GameEventsUI>
 {
+    #region serializable variables
+    [SerializeField] GameEventButtonPrefab _gameEventButtonPrefab;
+    [SerializeField] Transform _gameEventButtonParentTransform;
+    #endregion
 
+    #region local variables
+    Game _game;
+    List<GameEventButtonPrefab> _gameEventButtonPrefabs = new List<GameEventButtonPrefab>();
+    List<GameEventInfo> _gameEventInfos = new List<GameEventInfo>();
+    SceneReferences _sceneReferences;
+    #endregion
+
+    #region getters and setters
+    Game Game { get { if (_game == null) { _game = Game.Instance; } return _game; } }
+    SceneReferences SceneReferences { get { if (_sceneReferences == null) { _sceneReferences = SceneReferences.Instance; }return _sceneReferences; } }
+    #endregion
+
+    #region public methods
+    public void ClickEventsButton()
+    {
+        bool buttonsActive = _gameEventButtonParentTransform.gameObject.activeSelf;
+        _gameEventButtonParentTransform.gameObject.SetActive(!buttonsActive);
+        if (!buttonsActive)
+        {
+            foreach (GameEventInfo eventInfo in _gameEventInfos)
+            {
+                GameEventButtonPrefab gameEventButtonPrefab = Instantiate(_gameEventButtonPrefab, _gameEventButtonParentTransform);
+                gameEventButtonPrefab.Init(eventInfo);
+                _gameEventButtonPrefabs.Add(gameEventButtonPrefab);
+            }
+        }
+        else
+        {
+            foreach (GameEventButtonPrefab gameEventButtonPrefab in _gameEventButtonPrefabs)
+            {
+                Destroy(gameEventButtonPrefab.gameObject);
+            }
+            _gameEventButtonPrefabs.Clear();
+        }
+    }
+    public void Init()
+    {
+        foreach (Seal seal in Game.Seals)
+        {
+            if(seal.RescueProgress == SealRescueProgress.Rescue)
+            {
+                GameEventInfo gameEventInfo = new GameEventInfo(
+                    $"A {SceneReferences.GetDisplayName(seal.SealSpecies)} has been found and is in need of rescue.", 
+                    "Seal Rescue", 
+                    GameEventType.SealSpotted, seal);
+                _gameEventInfos.Add(gameEventInfo);
+            }
+        }
+    }
+    #endregion
 }
 
 public class GameEventInfo
@@ -12,6 +66,8 @@ public class GameEventInfo
     #region local variables
     string _eventDescription;
     string _eventName;
+    GameEventType _gameEventType;
+    Seal _seal;
     #endregion
 
     #region getters and setters
@@ -20,10 +76,12 @@ public class GameEventInfo
     #endregion
 
     #region constructors
-    public GameEventInfo(string eventDescription, string eventName)
+    public GameEventInfo(string eventDescription, string eventName, GameEventType gameEventType, Seal seal = null)
     {
         _eventDescription = eventDescription;
         _eventName = eventName;
+        _gameEventType = gameEventType;
+        _seal = seal;
     }
     #endregion
 }
