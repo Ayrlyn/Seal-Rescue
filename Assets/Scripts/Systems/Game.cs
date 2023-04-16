22800c42bd6a1c64e9f26f814bf1130e76b03035
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-04-16 22:03:24 +0100

    [MOD]Game can be progressed further

--- Assets/Scripts/Systems/Game.cs
+++ Assets/Scripts/Systems/Game.cs
@@ -1,31 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : Singleton<Game>, ISave<GameSave>
 {
+    #region serializable variables
+    [Header("Prefabs")]
+    [SerializeField] EmployeeIconPrefab _employeeIconPrefab;
+    [SerializeField] SealIconPrefab _sealIconPrefab;
+    #endregion
+
     #region local variables
+    HashSet<Employee> _employees = new HashSet<Employee>();
+    Dictionary<Employee, EmployeeIconPrefab> _employeesAndPrefabs = new Dictionary<Employee, EmployeeIconPrefab>();
     GameDateTime _gameDateTime;
     GameEventController _gameEventController;
     GameEventsUI _gameEventsUI;
     HashSet<string> _oneOffGameEvents = new HashSet<string>();
     Resources _resources;
     SaveManager _saveManager;
+    SceneReferences _sceneReferences;
     SealHospital _sealHospital;
     HashSet<Seal> _seals = new HashSet<Seal>();
+    Dictionary<Seal, SealIconPrefab> _sealsAndPrefabs = new Dictionary<Seal, SealIconPrefab>();
     UpkeepController _upkeepController;
     #endregion
 
     #region getters and setters
-    GameDateTime GameDateTime { get { if (_gameDateTime == null) { _gameDateTime = GameDateTime.Instance; } return _gameDateTime; } }
-    GameEventController GameEventController { get { if (_gameEventController == null) { _gameEventController = GameEventController.Instance; } return _gameEventController; } }
-    GameEventsUI GameEventsUI { get { if (_gameEventsUI == null) { _gameEventsUI = GameEventsUI.Instance; } return _gameEventsUI; } }
+    public HashSet<Employee> Employees { get { return _employees; } set { _employees = value; } }
+    public GameDateTime GameDateTime { get { if (_gameDateTime == null) { _gameDateTime = GameDateTime.Instance; } return _gameDateTime; } }
+    public GameEventController GameEventController { get { if (_gameEventController == null) { _gameEventController = GameEventController.Instance; } return _gameEventController; } }
+    public GameEventsUI GameEventsUI { get { if (_gameEventsUI == null) { _gameEventsUI = GameEventsUI.Instance; } return _gameEventsUI; } }
     public HashSet<string> OneOffGameEvents { get { return _oneOffGameEvents; } set { _oneOffGameEvents = value; } }
-    Resources Resources { get { if (_resources == null) { _resources = Resources.Instance; } return _resources; } }
-    SaveManager SaveManager { get { if (_saveManager == null) { _saveManager = SaveManager.Instance; } return _saveManager; } }
-    SealHospital SealHospital { get { if (_sealHospital == null) { _sealHospital = SealHospital.Instance; } return _sealHospital; } }
+    public Resources Resources { get { if (_resources == null) { _resources = Resources.Instance; } return _resources; } }
+    public SaveManager SaveManager { get { if (_saveManager == null) { _saveManager = SaveManager.Instance; } return _saveManager; } }
+    public SceneReferences SceneReferences { get { if(_sceneReferences == null) { _sceneReferences = SceneReferences.Instance; } return _sceneReferences; } }
     public HashSet<Seal> Seals { get { return _seals; } set { _seals = value; } }
-    UpkeepController UpkeepController { get { if (_upkeepController == null) { _upkeepController = UpkeepController.Instance; } return _upkeepController; } }
+    public UpkeepController UpkeepController { get { if (_upkeepController == null) { _upkeepController = UpkeepController.Instance; } return _upkeepController; } }
     #endregion
 
     #region unity methods
@@ -33,7 +45,8 @@ public class Game : Singleton<Game>, ISave<GameSave>
     {
         SaveManager.Load();
         GameEventController.Init();
-        SealHospital.Init();
+        SceneReferences.SealHospital.Init();
+        SceneReferences.VisitorCentre.Init();
         UpkeepController.Init();
         GameEventsUI.Init();
     }
@@ -41,6 +54,75 @@ public class Game : Singleton<Game>, ISave<GameSave>
     void OnApplicationQuit()
     {
         SaveManager.Save();
+    }
+    #endregion
+
+    #region local methods
+    void GenerateEmployee(Employee employee)
+    {
+        Employees.Add(employee);
+        EmployeeIconPrefab iconPrefab = Instantiate(_employeeIconPrefab);
+        iconPrefab.Init(employee);
+        if (!_employeesAndPrefabs.ContainsKey(employee)) { _employeesAndPrefabs.Add(employee, iconPrefab); }
+        else if(_employeesAndPrefabs[employee] == null) { _employeesAndPrefabs[employee] = iconPrefab; }
+
+        switch (employee.Location)
+        {
+            case "SealHospital":
+                SceneReferences.SealHospital.ReceiveEmployee(iconPrefab);
+                break;
+            case "VisitorCentre":
+                SceneReferences.VisitorCentre.ReceiveEmployee(iconPrefab);
+                break;
+            default:
+                break;
+        }
+        iconPrefab.transform.localPosition = Vector3.zero;
+        iconPrefab.transform.localScale = Vector3.one;
+    }
+
+    void GenerateSeal(Seal seal)
+    {
+        Seals.Add(seal);
+        SealIconPrefab iconPrefab = Instantiate(_sealIconPrefab);
+        Button button = iconPrefab.GetComponent<Button>();
+        iconPrefab.Init(seal);
+        if (!_sealsAndPrefabs.ContainsKey(seal)) { _sealsAndPrefabs.Add(seal, iconPrefab); }
+        else if(_sealsAndPrefabs[seal] == null) { _sealsAndPrefabs[seal] = iconPrefab; }
+
+        switch (seal.RescueProgress)
+        {
+            case SealRescueProgress.Arrival:
+                iconPrefab.transform.SetParent(SceneReferences.SealHospitalParent, false);
+                break;
+            case SealRescueProgress.Quarantine:
+                iconPrefab.transform.SetParent(SceneReferences.SealHospitalParent, false);
+                break;
+            case SealRescueProgress.TubeFeeding:
+                break;
+            case SealRescueProgress.ForceFeeding:
+                break;
+            case SealRescueProgress.HandFeeding:
+                break;
+            case SealRescueProgress.FishSchool:
+                break;
+            case SealRescueProgress.FreeFeeding:
+                break;
+            case SealRescueProgress.NurseryPool:
+                break;
+            case SealRescueProgress.RockPool:
+                break;
+            case SealRescueProgress.PhysioPool:
+                break;
+            case SealRescueProgress.PreReleasePool:
+                break;
+            case SealRescueProgress.Release:
+                break;
+            default:
+                break;
+        }
+        iconPrefab.transform.localPosition = Vector3.zero;
+        button.onClick.AddListener(() => SceneReferences.SealInfoPanel.ShowSeal(seal));
     }
     #endregion
 
@@ -52,11 +134,16 @@ public class Game : Singleton<Game>, ISave<GameSave>
         {
             OneOffGameEvents.Add(gameEvent);
         }
+        foreach (EmployeeSave employeeSave in state._employeeSaves)
+        {
+            Employee loadedEmployee = new Employee(employeeSave);
+            GenerateEmployee(loadedEmployee);
+        }
         Resources.Load(state._resourcesSave);
         foreach (SealSave sealSave in state._sealSaves)
         {
             Seal loadedSeal = new Seal(sealSave);
-            Seals.Add(loadedSeal);
+            GenerateSeal(loadedSeal);
         }
         UpkeepController.Load(state._upkeepSave);
     }
@@ -64,6 +151,11 @@ public class Game : Singleton<Game>, ISave<GameSave>
     public GameSave Save()
     {
         DateTimeSave dateTimeSave = GameDateTime.Save();
+        List<EmployeeSave> savedEmployees = new List<EmployeeSave>();
+        foreach (Employee employee in Employees)
+        {
+            savedEmployees.Add(employee.Save());
+        }
         ResourcesSave resourcesSave = Resources.Save();
         List<SealSave> savedSeals = new List<SealSave>();
         foreach (Seal seal in Seals)
@@ -71,7 +163,7 @@ public class Game : Singleton<Game>, ISave<GameSave>
             savedSeals.Add(seal.Save());
         }
         UpkeepSave upkeepSave = UpkeepController.Save();
-        return new GameSave(dateTimeSave, OneOffGameEvents, resourcesSave, savedSeals, upkeepSave);
+        return new GameSave(dateTimeSave, savedEmployees, OneOffGameEvents, resourcesSave, savedSeals, upkeepSave);
     }
     #endregion
 }
