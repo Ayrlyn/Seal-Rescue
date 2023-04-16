34863f5748ef107d58564fbc6b8a5bcfd6fadcc6
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-04-16 22:03:41 +0100

    [MOD]Expand Task functionality

--- Assets/Scripts/Entities/Task.cs
+++ Assets/Scripts/Entities/Task.cs
@@ -5,27 +5,92 @@ using UnityEngine;
 public class Task
 {
     #region local variables
-    int _minutesRemaining;
+    float _minutesRemaining;
     int _minutesRequired;
-    int _quantityRequired;
-    Resources _resourceRequired;
+    List<KeyValuePair<ResourceTypes, int>> _resourcesRequired;
     Seal _seal;
+    TaskType _taskType;
     #endregion
 
     #region getters and setters
-    public int MinutesRemaining { get { return _minutesRemaining; } set { _minutesRemaining = value; } }
+    public float MinutesRemaining { get { return _minutesRemaining; } set { _minutesRemaining = value; } }
     public int MinutesRequired { get { return _minutesRequired; } }
-    public int QuantityRequired { get { return _quantityRequired; } }
-    public Resources ResourceRequired { get { return _resourceRequired; } }
+    public List<KeyValuePair<ResourceTypes, int>> ResourcesRequired { get { return _resourcesRequired; } }
     public Seal Seal { get { return _seal; } }
+    public TaskType TaskType { get { return _taskType; } }
     #endregion
 
-    public Task(int minutesRequired, int quantityRequired, Resources resourceRequired, Seal seal = null)
+    #region constructors
+    public Task()
+    {
+
+    }
+
+    public Task(int minutesRequired, List<KeyValuePair<ResourceTypes, int>> resourcesRequired, TaskType taskType, Seal seal = null)
     {
         _minutesRemaining = minutesRequired;
         _minutesRequired = minutesRequired;
-        _quantityRequired = quantityRequired;
-        _resourceRequired = resourceRequired;
+        _resourcesRequired = resourcesRequired;
+        _taskType = taskType;
         _seal = seal;
     }
+    #endregion
+
+    #region public methods
+    public bool BonusProgress(List<Employee> employees)
+    {
+        foreach (Employee employee in employees)
+        {
+            switch (_taskType)
+            {
+                case TaskType.Clean: 
+                    if (employee.Specialty == WorkerSkills.Handy) { return true; }
+                    break;
+                case TaskType.Maintenance:
+                    if (employee.Specialty == WorkerSkills.Handy) { return true; }
+                    break;
+                case TaskType.Tourism:
+                    if (employee.Specialty == WorkerSkills.Community) { return true; }
+                    break;
+                case TaskType.TreatIllness:
+                    if (employee.Specialty == WorkerSkills.Medicine) { return true; }
+                    break;
+                case TaskType.TreatInjury:
+                    if (employee.Specialty == WorkerSkills.Medicine) { return true; }
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return false;
+    }
+
+    public void CompleteTask()
+    {
+        foreach (KeyValuePair<ResourceTypes, int> cost in ResourcesRequired)
+        {
+            Resources.Instance.SpendResource(cost.Key, cost.Value);
+        }
+        switch (_taskType)
+        {
+            case TaskType.Clean:
+                break;
+            case TaskType.Feed:
+                _seal.Feed();
+                break;
+            case TaskType.Maintenance:
+                break;
+            case TaskType.Tourism:
+                break;
+            case TaskType.TreatIllness:
+                _seal.TreatIllness();
+                break;
+            case TaskType.TreatInjury:
+                _seal.TreatWound();
+                break;
+            default:
+                break;
+        }
+    }
+    #endregion
 }
