1c1471e297584146f0f33fbaf69a7f729da3ce62
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-04-17 17:38:18 +0100

    [ADD]Task text decriptions

--- Assets/Scripts/UI/TaskUI.cs
+++ Assets/Scripts/UI/TaskUI.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TaskUI : MonoBehaviour
 {
-	#region serialiazable variables
+    #region serialiazable variables
+    [SerializeField] GameObject _infoPopup;
+    [SerializeField] TMP_Text _popupResources;
+    [SerializeField] TMP_Text _popupTimeRemaining;
+    [SerializeField] TMP_Text _popupTitle;
 	[SerializeField] Image _progressBar;
 	[SerializeField] Image _taskIcon;
     #endregion
@@ -21,7 +26,7 @@ public class TaskUI : MonoBehaviour
     void Update()
     {
         if(_task != null) { _progressBar.fillAmount = (float)_task.MinutesRemaining / (float)_task.MinutesRequired; }
-        
+        _popupTimeRemaining.text = $"Time Remaining: {(int)_task.MinutesRemaining}";
     }
     #endregion
 
@@ -35,6 +40,28 @@ public class TaskUI : MonoBehaviour
 		_progressBar.fillAmount = 1f;
         _task = task;
         if(_task.Seal != null) { _taskIcon.sprite = _task.Seal.IconPup; }
+    }
+
+    public void OnClick()
+    {
+        _infoPopup.SetActive(!_infoPopup.activeSelf);
+        if (!_infoPopup.activeSelf) { return; }
+
+        _popupTitle.text = _task.TaskType.ToString();
+
+        if(_task.ResourcesRequired.Count == 0) 
+        {
+            _popupResources.gameObject.SetActive(false);
+            return; 
+        }
+
+        string resourcesString = "Resources:";
+        foreach (KeyValuePair<ResourceTypes, int> resourceRequired in _task.ResourcesRequired)
+        {
+            resourcesString += $"\n - {resourceRequired.Value} {resourceRequired.Key.ToString()}";
+        }
+        _popupResources.gameObject.SetActive(true);
+        _popupResources.text = resourcesString;
     }
 	#endregion
 
