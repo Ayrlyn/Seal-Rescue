49e70700ddf921323f22e5caa47d541189fc978b
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-04-17 08:54:46 +0100

    [ADD]Get random element to list extension methods

--- Assets/Scripts/Utility/Extension Methods/ListExtensionMethods.cs
+++ Assets/Scripts/Utility/Extension Methods/ListExtensionMethods.cs
@@ -2,6 +2,11 @@ using System.Collections.Generic;
 
 public static class ListExtensionMethods
 {
+    public static T GetRandomElement<T>(this List<T> list)
+    {
+        int randomIndex = UnityEngine.Random.Range(0, list.Count);
+        return list[randomIndex];
+    }
     public static bool IsEmpty<T>(this List<T> list)
     {
         return list.Count == 0;
