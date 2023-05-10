70678c2ee12f205d63c0ad3c67d34d667a38bb4d
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-05-10 10:41:21 +0100

    [MOD]Expand UI appear and disappearing effects

--- Assets/Scripts/Utility/UIEffects.cs
+++ Assets/Scripts/Utility/UIEffects.cs
@@ -7,7 +7,11 @@ using static Easing;
 public enum uiEffect
 {
 	Appear,
-	Disappear
+	Disappear,
+    AppearFromBottom,
+    AppearFromTop,
+    DisappearToBottom,
+    DisappearToTop
 }
 public class UIEffects : MonoBehaviour
 {
@@ -17,6 +21,9 @@ public class UIEffects : MonoBehaviour
     #endregion
 
     #region local variables
+    float _startingX;
+    float _startingY;
+    float _startingZ;
     #endregion
 
     #region getters and setters
@@ -48,6 +55,10 @@ public class UIEffects : MonoBehaviour
     #region coroutines
     IEnumerator UIEffectRoutine(UIEffect uIEffect)
     {
+        yield return new WaitForSeconds(uIEffect.Delay);
+        float x = 0;
+        float y = 0;
+        float z = 0;
         switch (uIEffect.Effect)
         {
             case uiEffect.Appear:
@@ -56,10 +67,35 @@ public class UIEffects : MonoBehaviour
             case uiEffect.Disappear:
                 this.transform.localScale = Vector3.one;
                 break;
+            case uiEffect.AppearFromBottom:
+                _startingX = this.transform.position.x;
+                _startingY = this.transform.position.y;
+                _startingZ = this.transform.position.z;
+                y = 0;
+                this.transform.position = new Vector3(_startingX, y, _startingZ);
+                break;
+            case uiEffect.AppearFromTop:
+                _startingX = this.transform.position.x;
+                _startingY = this.transform.position.y;
+                _startingZ = this.transform.position.z;
+                y = Screen.height;
+                this.transform.position = new Vector3(_startingX, y, _startingZ);
+                break;
+            case uiEffect.DisappearToBottom:
+                _startingX = this.transform.position.x;
+                _startingY = this.transform.position.y;
+                _startingZ = this.transform.position.z;
+                this.transform.position = new Vector3(_startingX, _startingY, _startingZ);
+                break;
+            case uiEffect.DisappearToTop:
+                _startingX = this.transform.position.x;
+                _startingY = this.transform.position.y;
+                _startingZ = this.transform.position.z;
+                this.transform.position = new Vector3(_startingX, _startingY, _startingZ);
+                break;
             default:
                 break;
         }
-        yield return new WaitForSeconds(uIEffect.Delay);
         float v = 0;
         Function easingFunction = GetEasingFunction(uIEffect.EaseType);
         while (v < uIEffect.Duration)
@@ -71,6 +107,22 @@ public class UIEffects : MonoBehaviour
                     break;
                 case uiEffect.Disappear:
                     this.transform.localScale = Vector3.one * easingFunction(1, 0, v);
+                    break;
+                case uiEffect.AppearFromBottom:
+                    y =  easingFunction(0, _startingY, v);
+                    this.transform.position = new Vector3(_startingX, y, _startingZ);
+                    break;
+                case uiEffect.AppearFromTop:
+                    y = easingFunction(Screen.height, _startingY, v);
+                    this.transform.position = new Vector3(_startingX, y, _startingZ);
+                    break;
+                case uiEffect.DisappearToBottom:
+                    y = easingFunction(_startingY, 0, v);
+                    this.transform.position = new Vector3(_startingX, y, _startingZ);
+                    break;
+                case uiEffect.DisappearToTop:
+                    y = easingFunction(_startingY, Screen.height, v);
+                    this.transform.position = new Vector3(_startingX, y, _startingZ);
                     break;
                 default:
                     break;
@@ -85,6 +137,20 @@ public class UIEffects : MonoBehaviour
                 break;
             case uiEffect.Disappear:
                 this.gameObject.SetActive(false);
+                break;
+            case uiEffect.AppearFromBottom:
+                this.transform.position = new Vector3(_startingX, _startingY, _startingZ);
+                break;
+            case uiEffect.AppearFromTop:
+                this.transform.position = new Vector3(_startingX, _startingY, _startingZ);
+                break;
+            case uiEffect.DisappearToBottom:
+                this.gameObject.SetActive(false);
+                this.transform.position = new Vector3(_startingX, _startingY, _startingZ);
+                break;
+            case uiEffect.DisappearToTop:
+                this.gameObject.SetActive(false);
+                this.transform.position = new Vector3(_startingX, _startingY, _startingZ);
                 break;
             default:
                 break;
