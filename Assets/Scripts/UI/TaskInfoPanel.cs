ad536a5a9d897b8cc3a76de819782c17b9bf4cf9
Alasdair-Reavey <70411395+Ayrlyn@users.noreply.github.com>
2023-05-28 15:13:00 +0100

    [ADD]TaskInfoPanel script

---
+++ Assets/Scripts/UI/TaskInfoPanel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskInfoPanel : MonoBehaviour
+{
+	#region serialiazable variables
+	#endregion
+
+	#region local variables
+	#endregion
+
+	#region getters and setters
+	#endregion
+
+	#region unity methods
+	#endregion
+
+	#region local methods
+	#endregion
+
+	#region public methods
+	#endregion
+
+	#region coroutines
+	#endregion
+}
